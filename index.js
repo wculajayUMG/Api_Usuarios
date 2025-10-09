13a28c6c4bbfb7b4e482b21193fe2c613fca2b5e
wculajayUMG <wculajaya@miumg.edu.gt>
2025-10-09 11:39:29 -0600

    modificar codigo

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
 
 
 const app = express();
+
+app.use(cors());
+app.use(morgan());
 app.use(express.json());
 
 app.listen(3000, ()=>{
